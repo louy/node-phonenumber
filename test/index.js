02beb8b93c7ef30760c8e00a7b766ecac111d251
Louy <me@l0uy.com>
2015-09-09 00:51:15 +0300

    Use "expect" tests

--- test/index.js
+++ test/index.js
@@ -1,4 +1,4 @@
-var should = require('chai').should(),
+var expect = require('chai').expect,
     phone = require('../lib/index.js'),
     phone_util = phone.PhoneNumberUtil.getInstance();
 
@@ -7,71 +7,71 @@ describe('#parse', function(){
         var fn = function() {
             phone_util.parse('123123123', 'GB');
         };
-        fn.should.not.throw();
+        expect(fn).to.not.throw();
     });
 
     it('throws an error with invalid numbers', function(){
         var fn = function() {
             phone_util.parse(null);
         };
-        fn.should.throw();
+        expect(fn).to.throw();
         
         var fn2 = function() {
             phone_util.parse('213sad');
         };
-        fn2.should.throw();
+        expect(fn2).to.throw();
         
         var fn3 = function() {
             phone_util.parse('4324+323');
         };
-        fn3.should.throw();
+        expect(fn3).to.throw();
     });
 });
     
 describe('#format', function(){
     it('parse MY phone number format', function(){
         var number = phone_util.parse('0139348815','MY');
-        phone_util.format(number, phone.PhoneNumberFormat.INTERNATIONAL).should.equal('+60 13-934 8815');
+        expect(phone_util.format(number, phone.PhoneNumberFormat.INTERNATIONAL)).to.equal('+60 13-934 8815');
     });
 
     it('parse UK phone number format', function(){
         var number = phone_util.parse('0845 313 66 66','GB');
-        phone_util.format(number, phone.PhoneNumberFormat.INTERNATIONAL).should.equal('+44 845 313 6666');
+        expect(phone_util.format(number, phone.PhoneNumberFormat.INTERNATIONAL)).to.equal('+44 845 313 6666');
     });
 
     it('parse an international phone number', function(){
         var number = phone_util.parse('0012422342353','GB');
-        phone_util.format(number, phone.PhoneNumberFormat.INTERNATIONAL).should.equal('+1 242-234-2353');
+        expect(phone_util.format(number, phone.PhoneNumberFormat.INTERNATIONAL)).to.equal('+1 242-234-2353');
     });
 
     it('format in RFC3966', function(){
         var number = phone_util.parse('7239048346','TR');
-        phone_util.format(number, phone.PhoneNumberFormat.RFC3966).should.equal('tel:+90-7239048346');
+        expect(phone_util.format(number, phone.PhoneNumberFormat.RFC3966)).to.equal('tel:+90-7239048346');
     });
 
     it('format in E164', function(){
         var number = phone_util.parse('5070000000','TR');
-        phone_util.format(number, phone.PhoneNumberFormat.E164).should.equal('+905070000000');
+        expect(phone_util.format(number, phone.PhoneNumberFormat.E164)).to.equal('+905070000000');
     });
 
     it('format to national', function(){
         var number = phone_util.parse('+90 507 000 0000');
-        phone_util.format(number, phone.PhoneNumberFormat.NATIONAL).should.equal('0507 000 0000');
+        expect(phone_util.format(number, phone.PhoneNumberFormat.NATIONAL)).to.equal('0507 000 0000');
     });
 });
 
 describe('#format', function(){
     it('validate number', function() {
-        phone_util.isValidNumber(
+        expect(phone_util.isValidNumber(
             phone_util.parse('23907','GB')
-        ).should.equal(false);
+        )).to.equal(false);
 
-        phone_util.isValidNumber(
+        expect(phone_util.isValidNumber(
             phone_util.parse('53453455','GB')
-        ).should.equal(false);
+        )).to.equal(false);
 
-        phone_util.isValidNumber(
+        expect(phone_util.isValidNumber(
             phone_util.parse('8453136666','GB')
-        ).should.equal(true);
+        )).to.equal(true);
     });
 });
